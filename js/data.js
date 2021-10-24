a48ecd331040d6689d65024ccf38209c0db5b17a
Rikin Baghadiya <rikbagha@publicisgroupe.net>
2021-10-24 12:24:58 +0530

    some of improvements discussed after first demo

--- js/data.js
+++ js/data.js
@@ -1,3 +1,14 @@
+
+const aboutCBSELink="https://www.cbse.gov.in/cbsenew/pedagogy.html";
+const officialCBSELink="http://cbseaff.nic.in/cbse_aff/schdir_Report/userview.aspx";
+const aboutICSELink="https://www.youtube.com/watch?v=4KEnXvl7kgs&feature=emb_logo";
+const officialICSELink="https://www.cisce.org/Locate.aspx";
+const aboutIBLink="https://www.youtube.com/watch?v=9LuuJa3t-m8";
+const officialIBLink="https://www.ibo.org/programmes/find-an-ib-school/";
+const aboutIGCSELink="https://www.cambridgeinternational.org/why-choose-us/parents-and-students/";
+const officialIGCSELink="https://www.cambridgeinternational.org/why-choose-us/find-a-cambridge-school/";
+const officialAKESILink="https://agakhanschools.org/India";
+
 $(window).load(function () {
   $.ajax({
     type: "GET",
@@ -18,17 +29,20 @@ $('#ddlCity').change(
   function() {
       var city= this.value;
       $('button').removeClass('active');
-      $('#btnCBSE').addClass('active');  
-      callApiAndPopulateBoardDetails(city,"CBSE");
+      $('#btnALL').addClass('active');  
+      callApiAndPopulateBoardDetails(city,"ALL");
   }
 );
 
 $('#btnCBSE').click(
   function() {
     $('button').removeClass('active');
+    $('#boardInfo').show();
     $(this).addClass('active');
       var city=$('#ddlCity').val();
       var board = $(this).text();
+      $("#aboutBoardLink").attr("href", aboutCBSELink);
+      $("#OfficialLink").attr("href", officialCBSELink);
       callApiAndPopulateBoardDetails(city,board);
   }
 );
@@ -37,9 +51,12 @@ $('#btnCBSE').click(
 $('#btnICSE').click(
   function() {
     $('button').removeClass('active');
+    $('#boardInfo').show();
     $(this).addClass('active');
       var city=$('#ddlCity').val();
       var board = $(this).text();
+      $("#aboutBoardLink").attr("href", aboutICSELink);
+      $("#OfficialLink").attr("href", officialICSELink);
       callApiAndPopulateBoardDetails(city,board);
   }
 );
@@ -48,9 +65,13 @@ $('#btnICSE').click(
 $('#btnIB').click(
   function() {
     $('button').removeClass('active');
+    $('#boardInfo').show();
     $(this).addClass('active');
       var city=$('#ddlCity').val();
       var board = $(this).text();
+      $('#boardInfo').show();
+      $("#aboutBoardLink").attr("href", aboutIBLink);
+      $("#OfficialLink").attr("href", officialIBLink);
       callApiAndPopulateBoardDetails(city,board);
   }
 );
@@ -62,6 +83,9 @@ $('#btnIGCSE').click(
     $(this).addClass('active');
       var city=$('#ddlCity').val();
       var board = $(this).text();
+      $('#boardInfo').show();
+      $("#aboutBoardLink").attr("href",aboutIGCSELink);
+      $("#OfficialLink").attr("href",officialIGCSELink);
       callApiAndPopulateBoardDetails(city,board);
   }
 );
@@ -70,9 +94,12 @@ $('#btnIGCSE').click(
 $('#btnAKESI').click(
   function() {
     $('button').removeClass('active');
+    $('#boardInfo').show();
     $(this).addClass('active');
       var city=$('#ddlCity').val();
       var board = $(this).text();
+      $('#boardInfo').show();
+      $("#OfficialLink").attr("href",officialAKESILink);
       callApiAndPopulateBoardDetails(city,board);
   }
 );
@@ -81,45 +108,58 @@ $('#btnAKESI').click(
 $('#btnALL').click(
   function() {
     $('button').removeClass('active');
+    $('#boardInfo').hide();
     $(this).addClass('active');
       var city=$('#ddlCity').val();
       var board = $(this).text();
-      callApiAndPopulateBoardDetails(city,'');
+      callApiAndPopulateBoardDetails(city,'ALL');
   }
 );
 
 
+//+"&page"+page+"&size="+size
 function callApiAndPopulateBoardDetails(city,board)
       {
         $('#tBody').empty();
         $('#lblBoard').text(board);
+
+        if(board == "ALL")board="";
+
         $('#imgLoading').show();
+        var url =  "https://akeb-api.herokuapp.com/api/schools?city="+city+"&curriculum="+board;
+        
         $.ajax({
           type: "GET",
-          url: "https://akeb-api.herokuapp.com/api/schools?city="+city+"&curriculum="+board,
+          url: url,
           dataType: "json",
           contentType: "application/json; charset=utf-8",
           success: function (result) {
             var number_of_rows = result.data.length;
             var number_of_cols = 4;
             var table_body = '<table border="1">';
+            
             for(var i=0;i<number_of_rows;i++){
     
               table_body+='<tr>';
-              table_body +='<td width="55%">';
+              table_body +='<td width="40%">';
               table_body +='<b>'+result.data[i].name+'</b>';
+              table_body +='</td>';
+              
+              table_body +='<td width="10%">';
+              table_body +='<b>'+result.data[i].curriculum+'</b>';
               table_body +='</td>';
               
               table_body +='<td width="20%">';
               table_body +='<span>'+(result.data[i].noOfStudents==null?'-':result.data[i].noOfStudents)+'</span>';
               table_body +='</td>';
               
-            
-              table_body +='<td width="12%">';
-              table_body +='<a href="https://maps.google.com?q=19.1472339,72.8345429"+result.data[i].latitude+","+result.data[i].longitude target="_blank"><img src="images/map-icon.png" /><span>Map</span></a>';
+              table_body +='<td width="15%">';
+              var mapURL="https://maps.google.com?q="+(result.data[i].latitude+","+result.data[i].longitude);
+              //              alert(mapURL);
+              table_body +='<a href="' + mapURL + '" target="_blank"><img src="images/map-icon.png" /><span>Map</span></a>';          
               table_body +='</td>';
               
-              table_body +='<td width="13%">';
+              table_body +='<td width="15%">';
               table_body +='<a href=""><img src="images/website-icon.svg" /><span>Website</span></a>'
               table_body +='</td>';
     
