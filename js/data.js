1312f6950fefc294092e767eca72f41cd325f1c2
Rikin <rikinb786@gmail.com>
2021-11-02 23:10:34 +0530

    ui changes to add pincode functionality

--- js/data.js
+++ js/data.js
@@ -8,16 +8,24 @@ const officialIBLink="https://www.ibo.org/programmes/find-an-ib-school/";
 const aboutIGCSELink="https://www.cambridgeinternational.org/why-choose-us/parents-and-students/";
 const officialIGCSELink="https://www.cambridgeinternational.org/why-choose-us/find-a-cambridge-school/";
 const officialAKESILink="https://agakhanschools.org/India";
+const GET_CITY_LIST_URL="https://akeb-api.herokuapp.com/api/schools/cities";
+const GET_SCHOOLS_URL="https://akeb-api.herokuapp.com/api/schools";
+
 const pageSize=10;
 var page=0;
 var totalRecords;
 var totalNoOfPages;
+var schoolData;
+var isSearchByPincode=false;
 $(window).load(function () {
   $.ajax({
     type: "GET",
-    url: "https://akeb-api.herokuapp.com/api/schools/cities",
+    url: GET_CITY_LIST_URL,
     dataType: "json",
     contentType: "application/json; charset=utf-8",
+    headers: {
+      "Authorization": "Bearer 5mdq74lzuk1w8xsobjvg",
+    },
     success: function (result) {
         $('#ddlCity').empty();
         $('#ddlCity').append("<option value='NA' disabled selected>Select city</option>");
@@ -28,15 +36,31 @@ $(window).load(function () {
     }
 });
 
-$('#ddlCity').change(
+
+$('#btnSearch').click(
   function() {
-      var city= this.value;
-      $('button').removeClass('active');
-      $('#btnALL').addClass('active');  
-      page=0;
-      callApiAndPopulateBoardDetails(city,"ALL",page,pageSize);
+
+    var pincode = document.getElementById("txtPincode").value;
+
+    if(pincode == ""){
+      isSearchByPincode = false;
+    }
+    else if(pincode != "" && pincode.length == 6){
+      isSearchByPincode = true;
+    }
+    else{
+      alert("Please enter valid pincode");
+      return;
+    }
+
+    var city= $("#ddlCity").val();
+    $('button').removeClass('active');
+    $('#btnALL').addClass('active');  
+    page=0;
+    callApiAndPopulateBoardDetails(city,"ALL");
   }
 );
+
 
 $('#btnCBSE').click(
   function() {
@@ -49,7 +73,7 @@ $('#btnCBSE').click(
       $("#aboutBoardLink").attr("href", aboutCBSELink);
       $("#OfficialLink").attr("href", officialCBSELink);
       page=0;
-      callApiAndPopulateBoardDetails(city,board,page,pageSize);
+      callApiAndPopulateBoardDetails(city,board);
   }
 );
 
@@ -65,7 +89,7 @@ $('#btnICSE').click(
       $("#aboutBoardLink").attr("href", aboutICSELink);
       $("#OfficialLink").attr("href", officialICSELink);
       page=0;
-      callApiAndPopulateBoardDetails(city,board,page,pageSize);
+      callApiAndPopulateBoardDetails(city,board);
   }
 );
 
@@ -82,7 +106,7 @@ $('#btnIB').click(
       $("#aboutBoardLink").attr("href", aboutIBLink);
       $("#OfficialLink").attr("href", officialIBLink);
       page=0;
-      callApiAndPopulateBoardDetails(city,board,page,pageSize);
+      callApiAndPopulateBoardDetails(city,board);
   }
 );
 
@@ -98,7 +122,7 @@ $('#btnIGCSE').click(
       $("#aboutBoardLink").attr("href",aboutIGCSELink);
       $("#OfficialLink").attr("href",officialIGCSELink);
       page=0;
-      callApiAndPopulateBoardDetails(city,board,page,pageSize);
+      callApiAndPopulateBoardDetails(city,board);
   }
 );
 
@@ -114,7 +138,7 @@ $('#btnAKESI').click(
       $('#boardInfo').show();
       $("#OfficialLink").attr("href",officialAKESILink);
       page=0;
-      callApiAndPopulateBoardDetails(city,board,page,pageSize);
+      callApiAndPopulateBoardDetails(city,board);
   }
 );
 
@@ -127,7 +151,7 @@ $('#btnALL').click(
       var city=$('#ddlCity').val();
       var board = $(this).text();
       page=0;
-      callApiAndPopulateBoardDetails(city,"",page,pageSize);
+      callApiAndPopulateBoardDetails(city,"");
   }
 );
 
@@ -137,7 +161,8 @@ $('#btnPrev').click(
     var city=$('#ddlCity').val();
     var board = $('#lblBoard').html();
     page--;
-    callApiAndPopulateBoardDetails(city,board,page,pageSize);
+    displayCurrentPageData();
+    enableDisablePagination();
   }
 );
 
@@ -147,25 +172,114 @@ $('#btnNext').click(
     var board = $('#lblBoard').html();
     
     page++;
-    callApiAndPopulateBoardDetails(city,board,page,pageSize);
+    displayCurrentPageData();
+    enableDisablePagination();
   }
 );
 
-//+"&page"+page+"&size="+size
+function displayCurrentPageData(){
+  
+  var currentPageData = schoolData.slice((page*3),((page+1)*3));
+
+  var table_body = '';
+  var number_of_rows = currentPageData.length;
+
+  for(var i=0;i<number_of_rows;i++){
+      
+    table_body+='<tr>';
+    table_body +='<td width="35%">';
+    table_body +='<b>'+currentPageData[i].name+'</b>';
+    table_body +='</td>';
+    
+    table_body +='<td width="10%">';
+    table_body +='<b>'+currentPageData[i].curriculum+'</b>';
+    table_body +='</td>';
+
+    $('#thDistance').hide();
+
+    if(isSearchByPincode){
+    $('#thDistance').show();  
+    var distance = (currentPageData[i].distance/1000);
+    if(distance>100) distance = "More than 100";
+
+    distance+=" KM";
+
+    table_body +='<td width="10%">';
+    table_body +='<b>'+distance+'</b>';
+    table_body +='</td>';
+    }
+    
+    table_body +='<td width="20%">';
+    table_body +='<span>'+(currentPageData[i].noOfStudents==null?'-':currentPageData[i].noOfStudents)+'</span>';
+    table_body +='</td>';
+    
+    table_body +='<td width="15%">';
+    var mapURL="https://maps.google.com?q="+(currentPageData[i].latitude+","+currentPageData[i].longitude);
+    //              alert(mapURL);
+    table_body +='<a href="' + mapURL + '" target="_blank"><img src="images/map-icon.png" /><span>Map</span></a>';          
+    table_body +='</td>';
+    
+    table_body +='<td width="15%">';
+    table_body +='<a href=""><img src="images/website-icon.svg" /><span>Website</span></a>'
+    table_body +='</td>';
+
+    table_body+='</tr>';
+  }
+    table_body+='</table>';
+   $('#tBody').html(table_body);
+}
+
+function enableDisablePagination(){
+  if(totalNoOfPages==1 || totalNoOfPages==0){
+    $("#btnPrev").attr("disabled", true);
+    $("#btnNext").attr("disabled", true);
+  }else if(page == (totalNoOfPages-1)){
+  $("#btnNext").attr("disabled", true);
+  $("#btnPrev").attr("disabled", false);
+  }else if(page == 0){
+    $("#btnPrev").attr("disabled", true);
+    $("#btnNext").attr("disabled", false);
+  }else{
+    $("#btnPrev").attr("disabled", false);
+    $("#btnNext").attr("disabled", false);
+  }
+}
+
+
 function callApiAndPopulateBoardDetails(city,board)
       {
         $('#tBody').empty();
         $('#lblBoard').text(board);
+        page = 0;
+
+        var pincode = document.getElementById("txtPincode").value;
 
         if(board == "ALL")board="";
         $('#imgLoading').show();
-        var url =  "https://akeb-api.herokuapp.com/api/schools?city="+city+"&curriculum="+board+"&page="+page+"&size="+pageSize;
+        
+        var url =  GET_SCHOOLS_URL+"?city="+city+"&curriculum="+board+"&page=0&size=100";
+
+        if(isSearchByPincode){
+          url+="&pincode="+pincode;
+        }
+
+        
         $.ajax({
           type: "GET",
           url: url,
           dataType: "json",
           contentType: "application/json; charset=utf-8",
+          headers: {
+            "Authorization": "Bearer 5mdq74lzuk1w8xsobjvg",
+          },
           success: function (result) {
+
+            if(result.errorMessage != null){
+              alert(result.errorMessage+"- List of Schools from Selected City will be displayed.");
+              document.getElementById("txtPincode").value="";
+              isSearchByPincode=false;
+            }
+
             totalRecords = result.totalElements;
             
             totalNoOfPages = Math.floor((totalRecords/pageSize));
@@ -173,60 +287,17 @@ function callApiAndPopulateBoardDetails(city,board)
             if(totalRecords%pageSize > 0){
               totalNoOfPages++;
             }
-            
-            if(totalNoOfPages==1 || totalNoOfPages==0){
-              $("#btnPrev").attr("disabled", true);
-              $("#btnNext").attr("disabled", true);
-            }else if(page == (totalNoOfPages-1)){
-            $("#btnNext").attr("disabled", true);
-            $("#btnPrev").attr("disabled", false);
-            }else if(page == 0){
-              $("#btnPrev").attr("disabled", true);
-              $("#btnNext").attr("disabled", false);
-            }else{
-              $("#btnPrev").attr("disabled", false);
-              $("#btnNext").attr("disabled", false);
-            }
 
-            var number_of_rows = result.data.length;
-        
-            var table_body = '';
+            schoolData = result.data;
+            displayCurrentPageData();
+            enableDisablePagination();
             
-            for(var i=0;i<number_of_rows;i++){
-                
-              table_body+='<tr>';
-              table_body +='<td width="40%">';
-              table_body +='<b>'+result.data[i].name+'</b>';
-              table_body +='</td>';
-              
-              table_body +='<td width="10%">';
-              table_body +='<b>'+result.data[i].curriculum+'</b>';
-              table_body +='</td>';
-              
-              table_body +='<td width="20%">';
-              table_body +='<span>'+(result.data[i].noOfStudents==null?'-':result.data[i].noOfStudents)+'</span>';
-              table_body +='</td>';
-              
-              table_body +='<td width="15%">';
-              var mapURL="https://maps.google.com?q="+(result.data[i].latitude+","+result.data[i].longitude);
-              //              alert(mapURL);
-              table_body +='<a href="' + mapURL + '" target="_blank"><img src="images/map-icon.png" /><span>Map</span></a>';          
-              table_body +='</td>';
-              
-              table_body +='<td width="15%">';
-              table_body +='<a href=""><img src="images/website-icon.svg" /><span>Website</span></a>'
-              table_body +='</td>';
-    
-              table_body+='</tr>';
-            }
-              table_body+='</table>';
-             $('#tBody').html(table_body);
              $('#imgLoading').hide();
           }
       });
       
       }
 
-  $(".loader").fadeOut("slow");
+    $(".loader").fadeOut("slow");
 });
 
